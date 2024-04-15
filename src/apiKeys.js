4a0e08df5857e9ae316bc00364a3dac630bb5070
Rahul Raj <rahul.r0644@gmail.com>
2024-04-15 14:20:46 +0530

    Update apiKeys.js

--- src/apiKeys.js
+++ src/apiKeys.js
@@ -1,4 +1,3 @@
-// Visit https://api.openweathermap.org & then signup to get our API keys for free
 module.exports = {
   key: '6ce665a4d6b84d5d271bd5b8c306f12c',
   base: "https://api.openweathermap.org/data/2.5/",
