ee02155de393c10a559c658da731b142a1317f64
Rahul Raj <rahul.r0644@gmail.com>
2024-04-15 14:21:33 +0530

    Update apiKeys.js

--- src/apiKeys.js
+++ src/apiKeys.js
@@ -1,4 +1,4 @@
 module.exports = {
   key: '6ce665a4d6b84d5d271bd5b8c306f12c',
-  base: "https://api.openweathermap.org/data/2.5/",
+  base: "https://api.openweathermap.org/data/2.5/", //usng openweather api
 };
